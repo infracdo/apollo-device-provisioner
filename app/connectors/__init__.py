c11a16c6343fc07db43ecff51cd9d50ddf2f74f6
infracdo <infracdo@gmail.com>
2026-08-25 20:00:28 +0800

    add redis connector to connectors package

--- app/connectors/__init__.py
+++ app/connectors/__init__.py
@@ -3,10 +3,12 @@ from app.connectors.ssh_connector import SSHConnector
 from app.connectors.telnet_connector import TelnetConnector
 from app.connectors.routeros_connector import RouterOSConnector
 from app.connectors.api_connector import APIConnector
+from app.connectors.redis_connector import RedisConnector
 
 __all__ = [
     "SSHConnector",
     "TelnetConnector",
     "RouterOSConnector",
     "APIConnector",
+    "RedisConnector",
 ]
